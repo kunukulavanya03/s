76818409c3347bf9ccbbb3212f38df973065f8bc
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-11-28 15:04:29 +0530

    🚀 Initial commit - Generated with FigmaToFullApp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+export default App;
